86c1ee8f861e87ef538f5c5c746a740e82768c1a
omejeemmanuel1 <omejeemmanuel046@yahoo.com>
2022-03-11 04:56:42 -0800

    Print alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+/* omejeemanuel1 */
+
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (successs)
+ **/
+int main(void)
+{
+char letter;
+
+for (letter = 'z'; letter >= 'a'; letter--)
+putchar(letter);
+
+putchar('\n');
+
+return (0);
+}
