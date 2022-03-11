27b87f066c67422d49bb1a4784ed244a968e009d
omejeemmanuel1 <omejeemmanuel046@yahoo.com>
2022-03-11 05:15:33 -0800

    Programs with digit comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+/* omejeemmanuel */
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Alwyays return 0 (success)
+**/
+int main(void)
+{
+int num;
+
+for (num = 0; num <= 9; num++)
+{
+	putchar((num % 10) + '0');
+	if (num == 9)
+	continue;
+	putchar(',');
+	putchar(' ');
+}
+
+putchar('\n');
+
+return (0);
+}
