c4e4d9a09a5b05d4157e1e00afd974a5c2212731
omejeemmanuel1 <omejeemmanuel046@yahoo.com>
2022-03-16 11:03:22 -0700

    Debugging

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -2,25 +2,25 @@
 #include <time.h>
 #include <stdlib.h>
 #include "main.h"
+
 /**
 * positive_or_negative - Test function that prints if integer
 *				is positive or negative
 * @i:Random time variable
 * Return: 0
 **/
+
 void positive_or_negative(int i)
 {
 
-	int num = i;
-
 	srand(time(0));
-	num = rand() - RAND_MAX / 2;
+	i = rand() - RAND_MAX / 2;
 
-	if (num > 0)
-		printf("%i is positive\n", num);
-	else if (num < 0)
-		printf("%i is negative\n", num);
+	if (i > 0)
+		printf("%i is positive\n", i);
+	else if (i < 0)
+		printf("%i is negative\n", i);
 	else
-		printf("%i is zero\n", num);
+		printf("%i is zero\n", i);
 
 }
