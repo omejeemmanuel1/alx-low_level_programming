ebb7ea6d9af948db8edc626586d8494e6cb4af1d
omejeemmanuel1 <Omejeemmanuel046@yahoo.com>
2022-04-28 19:00:30 +0000

    Return value of a bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,7 +10,11 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= sizeof(n) * 8)
+	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
-	return (-1);
+
+	if ((n & (1 << index)) == 0)
+		return (0);
+
+	return (1);
 }
