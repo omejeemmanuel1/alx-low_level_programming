7bfcf33dff863b4723e8650d15a3ab2abd989c97
omejeemmanuel1 <Omejeemmanuel046@yahoo.com>
2022-04-28 18:38:14 +0000

    Binary rep of a num

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -17,10 +17,10 @@ void _print_binary(unsigned long int n)
  * print_binary - print a number in binary
  * @n: the number to print
  */
-//void print_binary(unsigned long int n)
-//{
-//	if (n)
+void print_binary(unsigned long int n)
+{
+	if (n)
 		_print_binary(n);
-//	else
+	else
 		_putchar('0');
 }
